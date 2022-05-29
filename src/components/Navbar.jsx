943f18b6f061a84cc7cc4280a7c40578f98dc763
James Riggleman <james.riggleman@yahoo.com>
2022-05-29 13:53:31 +0800

    changed logo height

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt="Logo" style={{ width: "85px" }} />
+        <img src={Logo} alt="Logo" style={{ width: "80px" }} />
       </div>
 
       {/* menu */}
